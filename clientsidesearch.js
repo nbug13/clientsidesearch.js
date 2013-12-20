97e059acde16aae65adbd3acffee7db7d28e9fe5
nickb96 <nicholasbugeja96@gmail.com>
2013-12-20 10:36:36 +0100

    Update clientsidesearch.js

--- clientsidesearch.js
+++ clientsidesearch.js
@@ -6,6 +6,18 @@
  *       and is currently unstable and might also be intensive.
  */
 
+// NEW selector
+jQuery.expr[':'].Contains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+
+// OVERWRITES old selecor
+jQuery.expr[':'].contains = function(a, i, m) {
+  return jQuery(a).text().toUpperCase()
+      .indexOf(m[3].toUpperCase()) >= 0;
+};
+//end case insensitive force
 
 	var searchColor = 0;
 	
